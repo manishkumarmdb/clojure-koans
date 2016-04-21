cc293886deae08156c1e2c8615537d651ac5ff2e
manishkumarmdb <manishkumarmdb@gmail.com>
2016-04-21 15:17:16 +0530

    Add file 09 and modified file 08

--- 08_conditionals.clj
+++ 08_conditionals.clj
@@ -3,45 +3,74 @@
 
 (defn explain-exercise-velocity [exercise-term]
   (case exercise-term
-        :bicycling        "pretty fast"
-        :jogging          "not super fast"
-        :walking          "not fast at all"
-        "is that even exercise?"))
+    :bicycling        "pretty fast"
+    :jogging          "not super fast"
+    :walking          "not fast at all"
+    "is that even exercise?"))
 
 (meditations
-  "You will face many decisions"
-  (= __ (if (false? (= 4 5))
-          :a
-          :b))
+ "You will face many decisions"
+ (= __ (if (false? (= 4 5))
+         :a
+         :b))
 
-  "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
-          []))
+ (= :a (if (false? (= 4 5))
+         :a
+         :b))
 
-  "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
+ "Some of them leave you no alternative"
+ (= __ (if (> 4 3)
+         []))
+
+ (= [] (if (> 4 3)
+         []))
+
+ "And in such a situation you may have nothing"
+ (= __ (if (nil? 0)
+         [:a :b :c]))
+
+ (= nil (if (nil? 0)
           [:a :b :c]))
 
-  "In others your alternative may be interesting"
-  (= :glory (if (not (empty? ()))
-              :doom
-              __))
+ "In others your alternative may be interesting"
+ (= :glory (if (not (empty? ()))
+             :doom
+             __))
 
-  "You may have a multitude of possible paths"
-  (let [x 5]
-    (= :your-road (cond (= x __) :road-not-taken
-                        (= x __) :another-road-not-taken
-                        :else __)))
+ (= :glory (if (not (empty? ()))
+             :doom
+             :glory))
 
-  "Or your fate may be sealed"
-  (= 'doom (if-not (zero? __)
-          'doom
-          'more-doom))
+ "You may have a multitude of possible paths"
+ (let [x 5]
+   (= :your-road (cond (= x __) :road-not-taken
+                       (= x __) :another-road-not-taken
+                       :else __)))
 
-  "In case of emergency, go fast"
-  (= "pretty fast"
-     (explain-exercise-velocity __))
+ (let [x 5]
+   (= :your-road (cond (= x 10) :road-not-taken
+                       (= x 12) :another-road-not-taken
+                       :else :your-road)))
 
-  "But admit it when you don't know what to do"
-  (= __
-     (explain-exercise-velocity :watching-tv)))
+ "Or your fate may be sealed"
+ (= 'doom (if-not (zero? __)
+            'doom
+            'more-doom))
+
+ (= 'doom (if-not (zero? 1)
+            'doom
+            'more-doom))
+
+ "In case of emergency, go fast"
+ (= "pretty fast"
+    (explain-exercise-velocity __))
+
+ (= "pretty fast"
+    (explain-exercise-velocity :bicycling))
+
+ "But admit it when you don't know what to do"
+ (= __
+    (explain-exercise-velocity :watching-tv)))
+
+(= "is that even exercise?"
+   (explain-exercise-velocity :watching-tv)))
