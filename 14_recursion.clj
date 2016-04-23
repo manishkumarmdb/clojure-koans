03a25ec9fa6ccc46c3b79ab49f606317bac820be
manishkumarmdb <manishkumarmdb@gmail.com>
2016-04-23 15:33:25 +0530

    update solutions

--- 14_recursion.clj
+++ 14_recursion.clj
@@ -1,35 +1,35 @@
 (ns koans.14-recursion
   (:require [koan-engine.core :refer :all]))
 
-(defn is-even? [n]
-  (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
 
 (defn is-even? [n]
   (if (= n 0)
     true
     (not (is-even? (dec n)))))
 
-(defn is-even-bigint? [n]
-  (loop [n   n
-         acc true]
-    (if (= n 0)
-      __
-      (recur (dec n) (not acc)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
+
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         reversed ()]
+    (if (= () coll)
+      reversed
+      (recur (rest coll) (cons (first coll) reversed)))))
+
 
 (defn factorial [n]
-  __)
+  (loop [n   n
+         acc 1]
+    (if (= 0 n)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
